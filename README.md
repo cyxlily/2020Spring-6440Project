e4681023d67e31baed1f8fa731876ce55fa3b055
yuxincui <yuxin.cui@intel.com>
2021-06-30 16:04:42 +0800

    deploy guide

--- README.md
+++ README.md
@@ -1,10 +1,66 @@
 # 2020Spring-6440Project
 
 AWS Based Chat Sticker Maker Web Application
+>Graduate student project of 6440 Cloud Computing course
 
+This repository contains:
+
+emotion-detection-master: guide about training the Deep Learning emotion detection model
+sticker_api: emotion predict api using the model we get from folder emotion-detection-master
+sticker_client: python version client to test server
+sticker_server: server using api 
+
+## Table of Contents
+
+- [Demo](#demo)
+- [Setup](#setup)
+- [Reference](#reference)
+
+## Demo
 https://youtu.be/dVVOON9T3zc
 
+## Setup
+
+You can deploy it on your Ubuntu.
+
+1. Download repository
+
+```sh
+$ git clone https://github.com/cyxlily/2020Spring-6440Project.git
+$ cd 2020Spring-6440Project 
+```
+ 2. Setup API
+ 
+```sh
+$ docker pull cyxlily/sticker_api 
+$ docker run -it -p 5000:5000 -p 5001:5001 -v $PWD:/home/lily cyxlily/sticker_api
+```
+Test: On your Ubuntu browser, enter http://localhost:5000/, and you will see the API document.
+
+3. Setup server
+Open a new terminal on Ubuntu.
+
+```sh
+$ docker ps
+```
+Remember the docker id number you run in Step 2.
+```sh
+$ docker exec -it your_docker_id_number /bin/bash 
+```
+In docker, run server.
+```sh
+$ cd /home/lily/sticker_server 
+$ python no_database_app.py
+```
+Test: On your Ubuntu browser, enter http://localhost:5001/, and you will see the Sticker Maker web page. Upload your photo and get your sticker.
+
+
+Also, you can deploy it onto Cloud.
+
+
+## Reference
 https://github.com/elzisiou/K-means
 
 https://towardsdatascience.com/deploy-a-machine-learning-model-as-an-api-on-aws-43e92d08d05b
+
 
---
+++ sticker_server/no_database_app.py
@@ -0,0 +1,86 @@
+# -*-coding:utf-8-*-
+from flask import Flask, render_template, request, jsonify
+#from flask_uploads import configure_uploads, UploadSet
+import os, base64,sys,requests,json,traceback
+import base64
+import time
+from datetime import date
+import random
+import string
+#import psycopg2
+
+app = Flask(__name__)
+
+# config location of uploaded files
+#base = os.path.dirname(os.path.abspath(__file__))
+#base = os.path.join(base, 'static')
+#app.config['UPLOADS_DEFAULT_DEST'] = base
+#photo = UploadSet()
+#configure_uploads(app, photo)
+
+
+@app.route('/up_file', methods=['GET', 'POST'])  # accept and store images
+def up_file():
+    if request.method == "POST":
+        # receipt image
+        file_name = ''.join(random.sample(string.ascii_letters + string.digits, 8)) + '.png'
+ #       print(file_name)
+#        photo.save(request.files['file'], 'img1', file_name)  # save image
+#        sticker_img = send_req_to_API(file_name)
+        gen_dict = send_req_to_API(request.files['file'])
+        return jsonify(gen_dict)
+
+
+@app.route('/', methods=['get', 'post'])
+def index():
+    return render_template('image.html')
+
+
+def send_req_to_API(file_name):
+#    imgpath = 'static/files/img1/' + file_name
+#    with open(imgpath, 'rb') as fin:
+#    image_byte_client = fin.read()
+    image_byte_client = file_name.read()  # bytes
+    base64data_client = base64.b64encode(image_byte_client)  # bytes
+    strdata_client = base64data_client.decode('utf-8')  # str
+    json_dict_client = {"imgbase64": strdata_client}  # dict
+
+    json_dict_client = {"imgbase64": strdata_client}  # dict
+#    json_dict_client = {"imgbase64": file_name}
+    json_str_client = json.dumps(json_dict_client)  # str
+    try:
+
+        t0 = time.time()
+        request = requests.post(url='http://0.0.0.0:5000/api', data=json_str_client)
+        #request = requests.post(url='http://ApiDemo-env.eba-qyf98pxi.us-east-1.elasticbeanstalk.com/api',data=json_str_client)
+        t1 = time.time()
+        print('send time is %.3f' % (t1 - t0))
+
+        response = request.json()
+        print("response: ", response)
+        print(response['code'])
+        json_dict_server = response['data']
+        emotion = json_dict_server['emotion']
+        print("The image emotion: ", emotion)
+        img_client = json_dict_server['sticker']
+        # print(f"img {img_client}")
+        
+        sticker = "data:image/png;base64,"+img_client
+        bg_path = "/static/files/Background/"
+        background = bg_path + emotion + ".png"
+        gen_dict = {"sticker": sticker, "background":background, "emotion":emotion}
+        
+        return gen_dict
+        # print(img_client)
+
+        # base64data_server = img_client.encode('utf-8')
+        # image_byte_server = base64.b64decode(base64data_server)
+        # nparr = np.fromstring(image_byte_server, dtype=np.uint8)
+        # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        # cv2.imwrite('output_imgs/sticker.jpg', img)
+    except Exception as e:
+        print("exception: " + str(e))
+        traceback.print_exc(file=sys.stdout)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
